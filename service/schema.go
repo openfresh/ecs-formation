939261adc953445a1162e86342687b905bf056d6
stormcat24 <a.yamada24@gmail.com>
2015-09-28 11:40:30 +0900

    add keep_desired_count

--- service/schema.go
+++ service/schema.go
@@ -5,25 +5,24 @@ import (
 )
 
 type Cluster struct {
-
-	Name	string
-	Services	map[string]Service
+	Name     string
+	Services map[string]Service
 }
 
 type Service struct {
-
-	Name	string
-	TaskDefinition	string	`yaml:"task_definition"`
-	DesiredCount	int64	`yaml:"desired_count"`
-	LoadBalancers	[]LoadBalancer	`yaml:"load_balancers"`
-	Role	string	`yaml:"role"`
+	Name             string
+	TaskDefinition   string         `yaml:"task_definition"`
+	DesiredCount     int64          `yaml:"desired_count"`
+	KeepDesiredCount bool           `yaml:"keep_desired_count"`
+	LoadBalancers    []LoadBalancer `yaml:"load_balancers"`
+	Role             string         `yaml:"role"`
 }
 
 func (self *Service) FindLoadBalancerByContainer(conname string, port int64) *LoadBalancer {
 
 	for _, lb := range self.LoadBalancers {
 		if lb.ContainerName == conname &&
-		lb.ContainerPort == port {
+			lb.ContainerPort == port {
 			return &lb
 		}
 	}
@@ -42,10 +41,9 @@ func (self *Service) FindLoadBalancerByName(name string) *LoadBalancer {
 }
 
 type LoadBalancer struct {
-
-	Name	string	`yaml:"name"`
-	ContainerName	string	`yaml:"container_name"`
-	ContainerPort	int64	`yaml:"container_port"`
+	Name          string `yaml:"name"`
+	ContainerName string `yaml:"container_name"`
+	ContainerPort int64  `yaml:"container_port"`
 }
 
 func CreateServiceMap(data []byte) (map[string]Service, error) {
