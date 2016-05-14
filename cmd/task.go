5f2b045e19531078d29e2490a007ea6aeb162d3c
stormcat24 <stormcat24@stormcat.io>
2016-05-14 19:21:38 +0900

    add short description of command

--- cmd/task.go
+++ cmd/task.go
@@ -23,16 +23,11 @@ import (
 // taskCmd represents the task command
 var taskCmd = &cobra.Command{
 	Use:   "task",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	Short: "Manage task definition and control running task on Amazon ECS",
+	RunE: func(cmd *cobra.Command, args []string) error {
 		// TODO: Work your own magic here
 		fmt.Println("task called")
+		return nil
 	},
 }
 
