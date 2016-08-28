3ac019c22922689668978245ce194b19e9b886eb
stormcat24 <stormcat24@stormcat.io>
2016-08-28 15:01:30 +0900

    add elbv2 clients

--- client/elbv2/client.go
+++ client/elbv2/client.go
@@ -9,6 +9,14 @@ import (
 
 type Client interface {
 	DescribeLoadBalancers(names []string) (*elbv2.DescribeLoadBalancersOutput, error)
+	CreateRule(params *elbv2.CreateRuleInput) ([]*elbv2.Rule, error)
+	DeleteRule(ruleArn string) error
+	DescribeRule(params *elbv2.DescribeRulesInput) ([]*elbv2.Rule, error)
+	ModifyRule(params *elbv2.ModifyRuleInput) ([]*elbv2.Rule, error)
+	CreateTargetGroup(params *elbv2.CreateTargetGroupInput) ([]*elbv2.TargetGroup, error)
+	DeleteTargetGroup(targetGroupArn string) error
+	DescribeTargetGroup(params *elbv2.DescribeTargetGroupsInput) ([]*elbv2.TargetGroup, error)
+	ModifyTargetGroup(params *elbv2.ModifyTargetGroupInput) ([]*elbv2.TargetGroup, error)
 }
 
 type DefaultClient struct {
@@ -27,4 +35,92 @@ func (c DefaultClient) DescribeLoadBalancers(names []string) (*elbv2.DescribeLoa
 	}
 
 	return result, err
+}
+
+func (c DefaultClient) CreateRule(params *elbv2.CreateRuleInput) ([]*elbv2.Rule, error) {
+
+	result, err := c.service.CreateRule(params)
+	if util.IsRateExceeded(err) {
+		return c.CreateRule(params)
+	}
+
+	return result.Rules, err
+}
+
+func (c DefaultClient) DeleteRule(ruleArn string) error {
+
+	params := elbv2.DeleteRuleInput{
+		RuleArn: aws.String(ruleArn),
+	}
+
+	_, err := c.service.DeleteRule(&params)
+	if util.IsRateExceeded(err) {
+		return c.DeleteRule(ruleArn)
+	}
+
+	return err
+}
+
+func (c DefaultClient) DescribeRule(params *elbv2.DescribeRulesInput) ([]*elbv2.Rule, error) {
+
+	result, err := c.service.DescribeRules(params)
+	if util.IsRateExceeded(err) {
+		return c.DescribeRule(params)
+	}
+
+	return result.Rules, err
+}
+
+func (c DefaultClient) ModifyRule(params *elbv2.ModifyRuleInput) ([]*elbv2.Rule, error) {
+
+	result, err := c.service.ModifyRule(params)
+	if util.IsRateExceeded(err) {
+		return c.ModifyRule(params)
+	}
+
+	return result.Rules, err
+}
+
+func (c DefaultClient) CreateTargetGroup(params *elbv2.CreateTargetGroupInput) ([]*elbv2.TargetGroup, error) {
+
+	result, err := c.service.CreateTargetGroup(params)
+	if util.IsRateExceeded(err) {
+		return c.CreateTargetGroup(params)
+	}
+
+	return result.TargetGroups, err
+}
+
+func (c DefaultClient) DeleteTargetGroup(targetGroupArn string) error {
+
+	params := elbv2.DeleteTargetGroupInput{
+		TargetGroupArn: aws.String(targetGroupArn),
+	}
+
+	_, err := c.service.DeleteTargetGroup(&params)
+	if util.IsRateExceeded(err) {
+		return c.DeleteTargetGroup(targetGroupArn)
+	}
+
+	return err
+}
+
+func (c DefaultClient) DescribeTargetGroup(params *elbv2.DescribeTargetGroupsInput) ([]*elbv2.TargetGroup, error) {
+
+	result, err := c.service.DescribeTargetGroups(params)
+	if util.IsRateExceeded(err) {
+		return c.DescribeTargetGroup(params)
+	}
+
+	return result.TargetGroups, err
+}
+
+func (c DefaultClient) ModifyTargetGroup(params *elbv2.ModifyTargetGroupInput) ([]*elbv2.TargetGroup, error) {
+
+	result, err := c.service.ModifyTargetGroup(params)
+	if util.IsRateExceeded(err) {
+		return c.ModifyTargetGroup(params)
+	}
+
+	return result.TargetGroups, err
 }
--- client/elbv2/mock.go
+++ client/elbv2/mock.go
@@ -8,3 +8,42 @@ type MockClient struct {
 func (c MockClient) DescribeLoadBalancers(names []string) (*elbv2.DescribeLoadBalancersOutput, error) {
 	return nil, nil
 }
+
+func (c MockClient) CreateRule(params *elbv2.CreateRuleInput) ([]*elbv2.Rule, error) {
+	return []*elbv2.Rule{}, nil
+}
+
+func (c MockClient) DeleteRule(ruleArn string) error {
+
+	return nil
+}
+
+func (c MockClient) DescribeRule(params *elbv2.DescribeRulesInput) ([]*elbv2.Rule, error) {
+
+	return []*elbv2.Rule{}, nil
+}
+
+func (c MockClient) ModifyRule(params *elbv2.ModifyRuleInput) ([]*elbv2.Rule, error) {
+
+	return []*elbv2.Rule{}, nil
+}
+
+func (c MockClient) CreateTargetGroup(params *elbv2.CreateTargetGroupInput) ([]*elbv2.TargetGroup, error) {
+
+	return []*elbv2.TargetGroup{}, nil
+}
+
+func (c MockClient) DeleteTargetGroup(targetGroupArn string) error {
+
+	return nil
+}
+
+func (c MockClient) DescribeTargetGroup(params *elbv2.DescribeTargetGroupsInput) ([]*elbv2.TargetGroup, error) {
+
+	return []*elbv2.TargetGroup{}, nil
+}
+
+func (c MockClient) ModifyTargetGroup(params *elbv2.ModifyTargetGroupInput) ([]*elbv2.TargetGroup, error) {
+
+	return []*elbv2.TargetGroup{}, nil
+}
